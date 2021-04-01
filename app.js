696c6906df579011175fa51d9f21671bf44dc147
Adam <pabek92@gmail.com>
2021-04-02 01:44:06 +0200

    add

--- app.js
+++ app.js
@@ -15,6 +15,13 @@ app.use("/api/auth", require("./routers/auth"));
 app.use(passport.initialize());
 require("./midlleware/passport")(passport);
 
+if (process.env.NODE_ENV === "production") {
+  app.use("/", express.static(path.join(__dirname, "client", "build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const PORT = process.env.PORT || config.get("port") || 5000;
 async function start() {
   try {
