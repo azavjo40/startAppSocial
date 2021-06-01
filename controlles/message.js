00eb59fb280878ed11914fbc468e506794a32850
Azam <pabek92@gmail.com>
2021-06-01 09:06:31 +0200

    chat history and unread messages

--- controlles/message.js
+++ controlles/message.js
@@ -1,6 +1,7 @@
 const People = require('../models/auth')
 const Chat = require('../models/chatIo')
-const UnreadMsg = require('../models/unreadMsg')
+const ChatHistory = require('../models/chatHistory')
+
 module.exports.searchPeople = async (req, res) => {
   try {
     const peoples = await People.find()
@@ -10,34 +11,25 @@ module.exports.searchPeople = async (req, res) => {
   }
 }
 
-module.exports.chatRest = async (req, res) => {
+module.exports.getMessages = async (req, res) => {
   try {
-    const chats = await Chat.find({ chatId: req.params.id })
-    res.status(200).json(chats)
+    let chatHistory
+    if (req.params.id) {
+      chatHistory = await ChatHistory.find({ unitedId: req.params.id })
+      if (chatHistory[0]) {
+        const chats = await Chat.find({ chatId: chatHistory[0]._id })
+        res.status(200).json(chats)
+      }
+    }
   } catch (e) {
     console.log(e)
   }
 }
 
-module.exports.unreadMsg = async (req, res) => {
+module.exports.chatHistory = async (req, res) => {
   try {
-    const { chatId, unread } = req.body
-    const ubdate = {
-      unread,
-      chatId,
-    }
-    const findUnread = await UnreadMsg.find(chatId)
-    if (!findUnread) {
-      const unraedMsg = await UnreadMsg({ chatId, unread })
-      res.status(201)
-    } else {
-      await UnreadMsg.findByIdAndUpdate(
-        { chatId },
-        { $set: ubdate },
-        { new: true }
-      )
-      res.status(200)
-    }
+    console.log(req.params.id)
+    const chatHistory = await ChatHistory.find()
   } catch (e) {
     console.log(e)
   }
